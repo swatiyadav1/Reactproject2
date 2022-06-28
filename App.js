a062ce46fc0dc521c7b087ae19f375734e4972b3
swatiyadav1 <108142625+swatiyadav1@users.noreply.github.com>
2022-06-28 21:34:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./style.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import data from "./data";
+
+export default function App(){
+
+
+    const cards = data.map(item => {
+        return (
+
+            <Card 
+
+            key={item.id}
+            item={item}
+            // {...item}
+            // img={item.coverImg}
+            // rating={item.stats.rating}
+            // reviewCount={item.stats.reviewCount}
+            // location={item.location} 
+            // title={item.title}
+            // price={item.price}
+            // openSpots={item.openSpots}
+            
+            />
+
+        )
+    })
+    return(
+        <div>
+            <Navbar />
+            <Hero />
+            <section className="cards-list">
+            {cards}
+            </section>
+            </div>
+    )
+}
---
+++ components/Card.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../style.css";
+
+export default function Card(props){
+
+    let badgeText
+    if(props.item.openSpots === 0){
+        badgeText = "SOLD OUT"
+    }
+    else if(props.item.location === "Online"){
+        badgeText="ONLINE"
+    }
+
+return(
+<div className="card">
+    {badgeText && <div className="card--badge">{badgeText}</div>}
+    <img src={`../images/${props.item.coverImg}`} className="card--image"/>
+    <div className="card--stats">
+        <img src="../images/star.png" className="card--star" />
+        <span>{props.item.stats.rating}</span>
+        <span className="gray">({props.item.stats.reviewCount}) . </span>
+        <span className="gray">{props.item.location}</span>
+    </div>
+    <p>{props.item.title}</p>
+    <p><span className="bold">From ${props.item.price}</span> / person</p>
+</div>
+
+)
+
+
+
+}
